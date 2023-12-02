4a5ded9cab04a5b4083afced208ce11ad1fc0711
tiondominic <145018221+tiondominic@users.noreply.github.com>
2023-12-02 20:50:24 +0800

    Update TION_SmallestFactor.py

--- TION_SmallestFactor.py
+++ TION_SmallestFactor.py
@@ -1,10 +1,13 @@
 numberInput = eval(input("Enter a num >2: "))
 
-for number in range(2, numberInput + 1): #adding 1 so the end will be the same as the number inputted
-    remainder = numberInput % number
-    if remainder > 0:
-        continue
-    else:
-        break
+if numberInput > 1:
+    for number in range(2, numberInput + 1): #adding 1 so the end will be the same as the number inputted
+        remainder = numberInput % number
+        if remainder > 0:
+            continue
+        else:
+            break
+    print("The lowest factor is", number)
+else:
+    print("Invalid input")
 
-print("The lowest factor is", number)
